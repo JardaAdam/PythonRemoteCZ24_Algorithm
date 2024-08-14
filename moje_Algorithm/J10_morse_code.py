70297067b54d0122d434314aa4d4142169fdaa92
JardaAdam <jardys23@gmail.com>
2024-08-14 20:19:40 +0200

    nestihl jsem to takze dodelam z gitu

--- moje_Algorithm/J10_morse_code.py
+++ moje_Algorithm/J10_morse_code.py
@@ -12,8 +12,25 @@ MORSE_CODE = {'.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D',
               '---...': ':', '-.-.-.': ';', '-...-': '=', '.-.-.': '+', '-....-': '-', '..--.-': '_',
               '.-..-.': '"', '...-..-': '$', '.--.-.': '@', '...---...': 'SOS'}
 
-
+#TODO dodelat podle lektora
 def decode_morse(morse_code: str) -> str:
+    #print(f"morse_code: {morse_code}")
+
+    # odstanit zbytecne mezery na zacatku a na konci kodu
+    morse_code = morse_code.strip()
+    #print(f"morse_code.strip(): '{morse_code}'")
+
+    # rozdelim si zadani na jednotlive znaky
+    morse_word_list = morse_code.split('   ')
+    result = ''
+    morse_char_list = morse_code.split()
+    print(f"morse_char_list: {morse_char_list}")
+    # FIXME: osetrit tri mezery => oddelovac slov
+
+    # kazdy znam prevedu podle slovniku MORSE_CODE na pismeno
+
+    for morse_char in morse_char_list:
+        result
     pass
 
 
@@ -23,6 +40,6 @@ def decode_morse(morse_code: str) -> str:
 
 
 if __name__ == "__main__":
-    decode_morse('.... . -.--   .--- ..- -.. .')  ## => 'HEY JUDE'
+    print(f"'decode_morse('.... . -.--   .--- ..- -.. .')  ## => 'HEY JUDE'
     decode_morse(' . ')  ## => 'E'
     decode_morse('...   ---   ...')  ## => 'S O S'
