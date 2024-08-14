505af40caf67712b9a1d3835d298af6729f3c837
JardaAdam <jardys23@gmail.com>
2024-08-14 18:38:26 +0200

    pridal jsem popis ulohy

--- moje_Algorithm/J07_anagram.py
+++ moje_Algorithm/J07_anagram.py
@@ -1,5 +1,7 @@
 from typing import List
-
+#Napište funkci, která najde všechny anagramy (přesmyčky) daného slova z uvedeného seznamu.
+#Nalezená slova by měla být vrácena jako seznam.
+#Co je to anagram? Jedná se o dvě různá slova, která se skládají ze stejných písmen.
 
 def anagrams(word: str, lst_of_words: List[str]) -> List[str]:
     sorted_word = (sorted(word))
