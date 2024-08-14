d53785dfbd0cc77f3f7b57ad4d3ee549697b903e
JardaAdam <jardys23@gmail.com>
2024-08-14 18:39:00 +0200

    vytvoril jsem zadani ulohy c.3 z journey

---
+++ moje_Algorithm/J08_phone_number.py
@@ -0,0 +1,14 @@
+from typing import List
+#Napište funkci, která přijme seznam 12 celých čísel a vrátí řetězec ve formátu telefonního čísla.
+#Struktura funkce by měla vypadat takto:
+
+def create_phone_number(n: List[int]) -> str:
+
+
+
+
+
+
+
+    create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1])
+# => returns "(+12) 345-678-901"
