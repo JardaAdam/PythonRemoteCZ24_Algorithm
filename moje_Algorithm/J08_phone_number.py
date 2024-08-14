54b34d1481678a1d6f14e7e1508e6c015210bd14
JardaAdam <jardys23@gmail.com>
2024-08-14 20:26:51 +0200

    dodelaj jsem uvozovky do vystupu

--- moje_Algorithm/J08_phone_number.py
+++ moje_Algorithm/J08_phone_number.py
@@ -12,5 +12,6 @@ def create_phone_number(n: List[int]) -> str:
 
 
 if __name__ == "__main__":
-    print(create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
+    n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]
+    print(f'"{create_phone_number(n)}"')
 # => returns "(+12) 345-678-901"
