5e978222c4ecf08b62ac91d0cd22ab5a128579bc
JardaAdam <jardys23@gmail.com>
2024-08-18 22:04:14 +0200

    J12-> doupravil jsem a porovnal s petrem

--- moje_Algorithm/J12_roman_number.py
+++ moje_Algorithm/J12_roman_number.py
@@ -8,23 +8,51 @@ hodnota menší číslice se odečítá (např. IV = 4, IX = 9).
 
 rom2dec('MMXXIV') -> 2024
 """
-rom_numbers = \
-    {'I' : 1,
-     'V' : 5,
-     'X' : 10,
-     'L' : 50,
-     'C' : 100,
-     'D' : 500,
-     'M' : 1000}
+ROMAN = \
+    {'I': 1,
+     'V': 5,
+     'X': 10,
+     'L': 50,
+     'C': 100,
+     'D': 500,
+     'M': 1000}
+
+
 def rom2dec(romstr: str):
     result = 0
     for i in range(len(romstr)):
-        result += rom_numbers[romstr[i]]
+        result += ROMAN[romstr[i]]
         if i < len(romstr) - 1:
-            if rom_numbers[romstr[i]] < rom_numbers[romstr[i + 1]]:
-                result -= 2 * rom_numbers[romstr[i]]
+            if ROMAN[romstr[i]] < ROMAN[romstr[i + 1]]:
+                result -= 2 * ROMAN[romstr[i]]
     return result
 
+# Petrovo verze 
 
-print(f"'{rom2dec('MMXXIV')}'")
-print(rom2dec('MMXLI'))
+
+def rom2decII(rom):
+    reversed_rom = reversed(rom)
+    arab = 0
+    last_number = 0
+    for character in reversed_rom:
+        roman = ROMAN[character]
+        if roman < last_number:
+            arab -= roman
+        else:
+            arab += roman
+        last_number = roman
+    return arab
+
+if __name__ == '__main__':
+    print(f"'{rom2dec('MMXXVI')}'")
+    print(rom2dec('MMXLI'))
+
+    roman_numbers = ["MMXXIV", "DCIX", "IL", "VIII", "XIX"]
+    for roman_number in roman_numbers:
+        print(f"{roman_number} -> {rom2dec(roman_number)}")
+    print("rom2decII")
+    roman_numbers = ["MMXXIV", "DCIX", "IL", "VIII", "XIX"]
+    for roman_number in roman_numbers:
+        print(f"{roman_number} -> {rom2decII(roman_number)}")
+        
+# TODO testy
