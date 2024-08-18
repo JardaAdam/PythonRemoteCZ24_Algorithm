ff4b3ba98ca499a88f6af0412ebd2dfed85a14ee
JardaAdam <jardys23@gmail.com>
2024-08-18 22:11:52 +0200

    J11 upraveno a Pridano Petrovo reseni

--- moje_Algorithm/J11_prefix_find.py
+++ moje_Algorithm/J11_prefix_find.py
@@ -17,7 +17,28 @@ def longest_prefix(strs: list) -> str:
                 return ""
     return prefix
 
+# Petrovo reseni
+def longest_prefixI(words):
+    if len(words) == 0:
+        return ''
+    if len(words) == 1:
+        return words[0]
+    prefix = words[0]
+    for word in words[1:]:
+        while not word.startswith(prefix):
+            # print(f"prefix : '{prefix}'")
+            prefix = prefix[:-1]
+    return prefix
 
 if __name__ == "__main__":
     print(f"'{longest_prefix(['abba', 'abeceda', 'ababa'])}'") # -> 'ab'
     print(f"'{longest_prefix(['abba', 'abeceda', 'tata'])}'") # -> ''
+    print(f"'{longest_prefix(['abeba', 'abeceda', 'abeaba'])}'")
+    print(f"'{longest_prefix(['abba', 'abeceda', 'tata'])}'")
+    print(f"'{longest_prefix(["květina", "květ", "květen"])}'")
+    print("longest_prefixI")
+    print(f"'{longest_prefixI(['abba', 'abeceda', 'ababa'])}'")  # -> 'ab'
+    print(f"'{longest_prefixI(['abba', 'abeceda', 'tata'])}'")  # -> ''
+    print(f"'{longest_prefixI(['abeba', 'abeceda', 'abeaba'])}'")
+    print(f"'{longest_prefixI(['abba', 'abeceda', 'tata'])}'")
+    print(f"'{longest_prefixI(["květina", "květ", "květen"])}'")
