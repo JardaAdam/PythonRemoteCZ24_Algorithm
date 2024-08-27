bd3b0a867ddf095c03398d67b77083ef3273fe68
JardaAdam <jardys23@gmail.com>
2024-08-27 23:02:38 +0200

    popisoval jsem si jak co funguje u videa a snazil se pochopit co proc a jak

--- moje_Algorithm/J15_factorial.py
+++ moje_Algorithm/J15_factorial.py
@@ -12,14 +12,14 @@ Factorial:
 
 
 def fact(n: int) -> int:
-    if n < 0:
-        raise ValueError
-    if n == 0 or n == 1:
-        return 1
-    result = 1
-    for i in range(2, n+1):
-        result *= i
-    return result
+	if n < 0:
+		raise ValueError
+	if n == 0 or n == 1:
+		return 1
+	result = 1
+	for i in range(2, n+1):
+		result *= i
+	return result
 
 
 
@@ -32,14 +32,14 @@ Rekurzivní verze faktoriálu:
 1! = 1
 """
 
-
+# nevyhoda narocnost na pamet v mezipameti zustavaji vsechny predchozi Fact
 def fact_r(n: int) -> int:
-    print(f"Počítám faktoriál pro číslo {n}")  # radek ktery demonstruje jak pocita tato funkce
-    if n < 0:
-        raise ValueError
-    if n == 0 or n == 1:
-        return 1
-    return n * fact_r(n-1)
+	print(f"Počítám faktoriál pro číslo {n}")  # radek ktery demonstruje jak pocita tato funkce
+	if n < 0:
+		raise ValueError
+	if n == 0 or n == 1:
+		return 1
+	return n * fact_r(n-1)
 # postup vypoctu n = 5        5 * 24 (fac_r(4))-> 120
 #                             4 * 6  (fac_r(3))
 #                             3 * 2  (fac_r(2))
@@ -47,5 +47,5 @@ def fact_r(n: int) -> int:
 
 
 if __name__ == '__main__':
-    for i in range(10):
-        print(f"{i}! = {fact(i)} = {fact_r(i)}")
+	for i in range(10):
+		print(f"{i}! = {fact(i)} = {fact_r(i)}")
